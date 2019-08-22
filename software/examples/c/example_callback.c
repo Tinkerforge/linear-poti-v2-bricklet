2fba75276a3d4da57ff14a993018ee7d251b0a40
Matthias Bolte <matthias@tinkerforge.com>
2019-08-22 12:06:12 +0200

    c: Adapt to bindings changes

--- software/examples/c/example_callback.c
+++ software/examples/c/example_callback.c
@@ -33,7 +33,7 @@ int main(void) {
 	// Register position callback to function cb_position
 	linear_poti_v2_register_callback(&lp,
 	                                 LINEAR_POTI_V2_CALLBACK_POSITION,
-	                                 (void *)cb_position,
+	                                 (void (*)(void))cb_position,
 	                                 NULL);
 
 	// Set period for position callback to 0.25s (250ms) without a threshold
