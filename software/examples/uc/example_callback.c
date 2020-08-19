690cdb0b50a5512750b06ec8922e4281ee16330f
Erik Fleckstein <erik@tinkerforge.com>
2020-08-19 16:52:21 +0200

    Add uc examples.

---
+++ software/examples/uc/example_callback.c
@@ -0,0 +1,40 @@
+
+
+#include "bindings/hal_common.h"
+#include "bindings/bricklet_linear_poti_v2.h"
+
+#define UID "XYZ" // Change XYZ to the UID of your Linear Poti Bricklet 2.0
+
+void check(int rc, const char* msg);
+
+
+
+// Callback function for position callback
+void position_handler(TF_LinearPotiV2 *device, uint8_t position, void *user_data) {
+	(void)device; (void)user_data; // avoid unused parameter warning
+
+	tf_hal_printf("Position: %u °\n", position);
+}
+
+
+TF_LinearPotiV2 lp;
+
+void example_setup(TF_HalContext *hal) {
+	// Create device object
+	check(tf_linear_poti_v2_create(&lp, UID, hal), "create device object");
+
+
+	// Register position callback to function position_handler
+	tf_linear_poti_v2_register_position_callback(&lp,
+	                                            position_handler,
+	                                            NULL);
+
+	// Set period for position callback to 0.25s (250ms) without a threshold
+	tf_linear_poti_v2_set_position_callback_configuration(&lp, 250, false, 'x', 0, 0);
+
+}
+
+void example_loop(TF_HalContext *hal) {
+	// Poll for callbacks
+	tf_hal_callback_tick(hal, 0);
+}
---
+++ software/examples/uc/example_simple.c
@@ -0,0 +1,31 @@
+
+
+#include "bindings/hal_common.h"
+#include "bindings/bricklet_linear_poti_v2.h"
+
+#define UID "XYZ" // Change XYZ to the UID of your Linear Poti Bricklet 2.0
+
+void check(int rc, const char* msg);
+
+
+
+
+TF_LinearPotiV2 lp;
+
+void example_setup(TF_HalContext *hal) {
+	// Create device object
+	check(tf_linear_poti_v2_create(&lp, UID, hal), "create device object");
+
+
+	// Get current position
+	uint8_t position;
+	check(tf_linear_poti_v2_get_position(&lp, &position), "get position");
+
+	tf_hal_printf("Position: %u °\n", position);
+
+}
+
+void example_loop(TF_HalContext *hal) {
+	// Poll for callbacks
+	tf_hal_callback_tick(hal, 0);
+}
